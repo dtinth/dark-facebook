03369a152334b15df5a49500ed6d71d2228437d1
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2011-11-23 11:18:38 +0700

    script to publish CSS to userstyles.org

--- README.md
+++ README.md
@@ -15,8 +15,13 @@ Get the Userstyle
 * or download __dark-facebook.css__ from this repository and add to Stylish manually.
 
 
+
+
+
 Development Environment
 -----------------------
+
+I work on a Mac but maybe you can use it on Linux or Windows (maybe with Cygwin).
 
     node /path/to/LiveStyl/livestyl.js main.styl
 
@@ -24,6 +29,28 @@ And then go to `http://localhost:25531/` and run the bookmarklet on Facebook.
 Your changes will be applied when you save the file.
 
 To generate the final CSS, run `make` __with the livestyl server running__ and it will make __dark-facebook.css__.
+
+
+
+Publishing to Userstyles.org
+----------------------------
+
+In case you made a fork of it, you can use the provided script (written in PhantomJS) to submit your style to Userstyles.org.
+
+But you need to create `userstyles-credentials.json` and fill it with your login credentials:
+
+	{ "username": ".....", "password": "....." }
+
+You also need to alter `userstyles-config.json` to make it point to the right style ID. This means you need to
+create a style manually at userstyles.org first.
+
+	{ "styleID": "56731", "filename": "dark-facebook.css" }
+
+After that, the users of the userstyle can update to it.
+
+
+
+
 
 
 
---
+++ post-to-userstyles.js
@@ -0,0 +1,71 @@
+
+// userstyles-credentials.json: { "username": ".....", "password": "....." }
+// userstyles-config.json: { "styleID": "56731", "filename": "dark-facebook.css" }
+
+var fs = require('fs');
+try {
+	var credentials = JSON.parse(fs.read('userstyles-credentials.json'));
+	var config      = JSON.parse(fs.read('userstyles-config.json'));
+	var css = fs.read(config.filename);
+} catch (e) {
+	console.log('configuration read error: ' + e);
+	phantom.exit(1);
+}
+
+var editLink = 'http://userstyles.org/styles/' + config.styleID + '/edit';
+var page = new WebPage();
+
+function setGlobal(page, name, data) {
+	var json = JSON.stringify(data);
+	var fn = 'return window[' + JSON.stringify(name) + ']=' + json + ';';
+	return page.evaluate(new Function(fn));
+};
+
+page.onLoadFinished = processPage;
+page.open(editLink);
+page.onConsoleMessage = function (msg) { console.log('page : ' + msg); };
+
+var processState = 0;
+
+function processPage(status) {
+	setGlobal(page, '__dt_credentials', credentials);
+	setGlobal(page, '__dt_state', processState);
+	setGlobal(page, '__dt_css', css);
+	console.log(page.evaluate(function() { return location.href + ' // ' + document.title; }));
+	var retval = page.evaluate(function() {
+		try {
+			var un, pw, el;
+			if ((__dt_state != 1) && (un = document.querySelector('#login-existing')) && (pw = document.querySelector('#password-existing'))) {
+				un.value = __dt_credentials.username;
+				pw.value = __dt_credentials.password;
+				pw.form.submit();
+				console.log('form submitted');
+				return 1;
+			}
+			if ((__dt_state != 2) && (el = document.querySelector('textarea[name="style[css]"]#css'))) {
+				el.value = __dt_css;
+				if (el.value && el.value != 'undefined' && el.value != 'null') {
+					console.log('css submitted');
+					el.form.submit();
+				} else {
+					console.log('can\'t set css!');
+					return -1;
+				}
+				return 2;
+			}
+			if (document.querySelector('#style-info')) {
+				console.log('finished');
+				return -1;
+			}
+		} catch (e) {
+			console.log('error : ' + e);
+			return -1;
+		}
+		return __dt_state;
+	});
+	if (retval == -1) {
+		phantom.exit();
+	} else {
+		processState = retval;
+	}
+}
