68e9d5009bcea13829cf41d2078e2290f5e671ea
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2011-11-24 23:04:41 +0700

    main.styl: timeline tweaks

--- images/script.js
+++ images/script.js
@@ -3,23 +3,15 @@ var Canvas = require('canvas');
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 
-function loadImageFile(file) {
-	var image = new Canvas.Image()
-	image.src = fs.readFileSync(file);
-	var canvas = new Canvas(image.width, image.height);
-	var ctx = canvas.getContext('2d');
-	ctx.drawImage(image, 0, 0);
-	return canvas;
-}
-
-var alreadyRun = {};
-
 function log() {
 	var x = Array.prototype.slice.call(arguments);
 	for (var i = 0; i < log.depth; i ++) x.unshift(i == 0 ? '|-' : '| ');
 	console.log.apply(console, x);
 }
+
 log.depth = 0;
+
+var alreadyRun = {};
 
 function runTask(taskName, next) {
 	if (alreadyRun[taskName]) return next();
@@ -42,46 +34,9 @@ function runTask(taskName, next) {
 		loadImage(taskName, task, cont);
 	} else if (typeof task == 'function') {
 		task(taskName, cont);
+	} else if (task instanceof EffectChain) {
+		task.run(taskName, cont);
 	}
-}
-
-function runCommand(cmd, args, callback) {
-	var proc = spawn(cmd, args);
-	log('exec:', cmd, args.join(' '));
-	proc.stdout.on('data', function(data) {
-		process.stdout.write(data);
-	});
-	proc.stderr.on('data', function(data) {
-		process.stderr.write(data);
-	});
-	proc.stdin.end();
-	proc.on('exit', callback);
-}
-
-function loadImage(dest, src, next) {
-	try {
-		if (fs.statSync(dest)) {
-			log('image already loaded');
-			return next();
-		}
-	} catch (e) {
-		log('has to load image');
-	}
-	runCommand('curl', ['-s', src, '-o' + dest], function(code) {
-		if (code == 0) {
-			next();
-		} else {
-			throw new Error('failed to download image! code ' + code);
-		}
-	});
-}
-
-function runAll(next) {
-	var list = [];
-	for (var i in tasks) {
-		list.push(i);
-	}
-	runTaskList(list, next);
 }
 
 function runTaskList(list, next) {
@@ -101,73 +56,146 @@ function runTaskList(list, next) {
 	loop();
 }
 
-function iop(src) {
-	var list = [];
-	var index = 0;
-	function f(dest, next) {
-		function pass(img) {
-			if (index >= list.length) {
-				fs.writeFileSync(dest, img.toBuffer());
-				next();
-				return;
-			} else {
-				list[index](img, function(img2) {
-					index++;
-					pass(img2);
-				});
-			}
-		}
-		if (typeof src == 'string') {
-			runTask(src, function() {
-				pass(loadImageFile(src));
-			});
-		} else {
-			pass(src);
-		}
-	}
-	f.chain = function(c) {
-		list.push(c);
-		return f;
-	};
-	for (var k in t) {
-		(function(k) {
-			f[k] = function() {
-				return f.chain(t[k].apply(t, arguments));
-			};
-		})(k);
-	}
-	return f;
+function runCommand(cmd, args, callback) {
+	var proc = spawn(cmd, args);
+	log('exec:', cmd, args.join(' '));
+	proc.stdout.on('data', function(data) {
+		process.stdout.write(data);
+	});
+	proc.stderr.on('data', function(data) {
+		process.stderr.write(data);
+	});
+	proc.stdin.end();
+	proc.on('exit', callback);
 }
 
-var t = {
-	pixelManipulate: function(callback) {
-		return function(canvas, next) {
-			var ctx = canvas.getContext('2d');
-			var data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-			var l = data.data.length;
-			var percent = 0, written = 0;
-			var number = 0;
-			process.stderr.write('\r');
-			for (var i = 0; i < l; i += 4) {
-				percent = (i / l) * 50;
-				for (; written < percent; written++) process.stderr.write('*');
-				var pixel = [data.data[i], data.data[i + 1], data.data[i + 2], data.data[i + 3]];
-				callback(pixel, number % data.width, Math.floor(number / data.width));
-				number++;
-				data.data[i]     = pixel[0];
-				data.data[i + 1] = pixel[1];
-				data.data[i + 2] = pixel[2];
-				data.data[i + 3] = pixel[3];
+function runAll(next) {
+	var list = [];
+	for (var i in tasks) {
+		list.push(i);
+	}
+	runTaskList(list, next);
+}
+
+
+
+
+function EffectChain(source, filter) {
+	this._source = source;
+	this._filter = filter;
+}
+
+EffectChain.prototype = {
+	chain: function(filter) {
+		return new EffectChain(this, filter);
+	},
+	run: function(dest, callback) {
+		this._filter(this._source, dest, callback);
+	},
+	fetch: function(src) {
+		return this.chain(function(source, dest, next) {
+			var loaded = false;
+			try {
+				if (fs.statSync(dest)) {
+					log('image already loaded');
+					loaded = true;
+				}
+			} catch (e) {
 			}
-			process.stderr.write('\r');
-			for (; written >= 0; written --) process.stderr.write(' ');
-			process.stderr.write('\r');
-			ctx.putImageData(data, 0, 0);
-			next(canvas);
-		};
+			if (loaded) {
+				return next(dest);
+			}
+			log('has to load image');
+			runCommand('curl', ['-s', src, '-o' + dest], function(code) {
+				if (code == 0) {
+					next(dest);
+				} else {
+					throw new Error('failed to download image! code ' + code);
+				}
+			});
+		});
+	},
+	load: function(filename) {
+		return this.chain(function(source, dest, next) {
+			log('loading image: ' + filename);
+			runTask(filename, function() {
+				var image = new Canvas.Image()
+				image.src = fs.readFileSync(filename);
+				var canvas = new Canvas(image.width, image.height);
+				var ctx = canvas.getContext('2d');
+				ctx.drawImage(image, 0, 0);
+				next(canvas);
+			});
+		});
+	},
+	save: function() {
+		return this.chain(function(source, dest, next) {
+			source.run(dest, function(canvas) {
+				log('saving image to: ' + dest);
+				fs.writeFileSync(dest, canvas.toBuffer());
+				next(dest);
+			});
+		});
+	},
+	compress: function() {
+		return this.chain(function(source, dest, next) {
+			if (process.env.COMPRESS_PNG != '1') {
+				return source.run(dest, function(filename) {
+					log('compression disabled, export COMPRESS_PNG=1 to enable');
+					next(filename);
+				});
+			}
+			var tempnam = 'TMP-' + dest;
+			source.run(tempnam, function(outnam) {
+				if (typeof outnam != 'string') {
+					throw new Error('invalid output format. forgot to call save()?');
+				}
+				runCommand('pngout', [outnam, dest, '-y'], function(code) {
+					try {
+						if (code != 0) throw new Error('pngout returned ' + code);
+						fs.statSync(dest);
+						fs.unlinkSync(outnam);
+					} catch (e) {
+						log('!!! cannot compress png: ' + e);
+						log('    leaving as is');
+						fs.renameSync(outnam, dest);
+					}
+					next();
+				});
+			});
+		});
+	},
+	pixelManipulate: function(desc, callback) {
+		return this.chain(function(source, dest, next) {
+			source.run(dest, function(canvas) {
+				log('pixel manipulation: ' + desc);
+				var ctx = canvas.getContext('2d');
+				var data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+				var l = data.data.length;
+				var percent = 0, written = 0;
+				var number = 0;
+				process.stderr.write('\r');
+				for (var i = 0; i < l; i += 4) {
+					percent = (i / l) * 50;
+					for (; written < percent; written++) process.stderr.write('*');
+					var pixel = [data.data[i], data.data[i + 1], data.data[i + 2], data.data[i + 3]];
+					callback(pixel, number % data.width, Math.floor(number / data.width));
+					number++;
+					data.data[i]     = pixel[0];
+					data.data[i + 1] = pixel[1];
+					data.data[i + 2] = pixel[2];
+					data.data[i + 3] = pixel[3];
+				}
+				process.stderr.write('\r');
+				for (; written >= 0; written --) process.stderr.write(' ');
+				process.stderr.write('\r');
+				ctx.putImageData(data, 0, 0);
+				next(canvas);
+			});
+		});
 	},
 	crop: function(x1, y1, x2, y2) {
-		return t.pixelManipulate(function(pixel, x, y) {
+		return this.pixelManipulate('crop: (' + x1 + ',' + y1 + '), (' + x2 + ',' + y2 + ')', function(pixel, x, y) {
 			if (!((x1 <= x && x < x2) && (y1 <= y && y < y2))) {
 				pixel[0] = 0;
 				pixel[1] = 0;
@@ -176,49 +204,71 @@ var t = {
 			}
 		});
 	},
-	darken: function(rx, gx, bx) {
-		if (rx == null) rx = 1;
-		if (gx == null) gx = 1;
-		if (bx == null) bx = 1;
-		return t.pixelManipulate(function(pixel) {
-			var value = (pixel[0] + pixel[1] + pixel[2]) / 3 / 255;
-			value = 1 - value;
-			pixel[0] = Math.pow(value, rx) * 255;
-			pixel[1] = Math.pow(value, gx) * 255;
-			pixel[2] = Math.pow(value, bx) * 255;
+	colorize: function(o) {
+		function parse(c) {
+			return [parseInt(c.substr(0, 2), 16), parseInt(c.substr(2, 2), 16), parseInt(c.substr(4, 2), 16)];
+		}
+		function value(c) {
+			return Math.min(1, Math.max(0, (c[0] + c[1] + c[2]) / 3 / 255));
+		}
+		var l = [], len = 0;
+		for (var s in o) {
+			l[len++] = { source: value(parse(s)), dest: parse(o[s]) };
+		}
+		l.sort(function(a, b) {
+			return a.source - b.source;
+		});
+		function set(p, c) {
+			p[0] = c[0]; p[1] = c[1]; p[2] = c[2];
+		}
+		return this.pixelManipulate('colorize', function(pixel) {
+			var v = value(pixel);
+			if (v < l[0].source) return set(pixel, l[0].dest);
+			if (l[len - 1].source <= v) return set(pixel, l[len - 1].dest);
+			for (var i = 0; i < len - 1; i ++) {
+				if (l[i].source <= v && v < l[i + 1].source) {
+					var vv = (v - l[i].source) / (l[i + 1].source - l[i].source);
+					for (var j = 0; j < 3; j ++) {
+						pixel[j] = l[i].dest[j] * vv + l[i + 1].dest[j] * (1 - vv);
+					}
+				}
+			}
 		});
 	},
-	pngCompress: function(srcFunction) {
-		return function(dest, next) {
-			if (process.env.COMPRESS_PNG != '1') {
-				srcFunction(dest, function() {
-					log('compression disabled, export COMPRESS_PNG=1 to enable');
-					next();
-				});
-				return;
-			}
-			var tempnam = 'TMP-' + dest;
-			srcFunction(tempnam, function() {
-				runCommand('pngout', [tempnam, dest, '-y'], function(code) {
-					try {
-						if (code != 0) throw new Error('pngout returned ' + code);
-						fs.statSync(dest);
-						fs.unlinkSync(tempnam);
-					} catch (e) {
-						log('!!! cannot compress png: ' + e);
-						log('    leaving as is');
-						fs.renameSync(tempnam, dest);
-					}
-					next();
-				});
-			});
-		};
+	darken: function(rx, gx, bx) {
+		return this.grayscale().invert();
+	},
+	grayscale: function() {
+		return this.pixelManipulate('grayscale', function(pixel) {
+			var value = (pixel[0] + pixel[1] + pixel[2]) / 3;
+			pixel[0] = pixel[1] = pixel[2] = value;
+		});
+	},
+	invert: function() {
+		return this.pixelManipulate('invert', function(pixel) {
+			pixel[0] = 255 - pixel[0];
+			pixel[1] = 255 - pixel[1];
+			pixel[2] = 255 - pixel[2];
+		});
+	},
+	curve: function(rx, gx, bx) {
+		return this.pixelManipulate(function(pixel) {
+			pixel[0] = Math.pow(pixel[0] / 255, rx);
+			pixel[1] = Math.pow(pixel[1] / 255, gx);
+			pixel[2] = Math.pow(pixel[2] / 255, bx);
+		});
 	}
 };
 
+var fx = new EffectChain();
+
 var tasks = {
-	"fb-sprite1.png": "https://s-static.ak.facebook.com/rsrc.php/v1/yj/r/HqOfhywhWvc.png",
-	"stream-button.png": iop("fb-sprite1.png").darken().crop(0, 454, 64, 480)
+	"fb-sprite1.png":      fx.fetch("https://s-static.ak.facebook.com/rsrc.php/v1/yj/r/HqOfhywhWvc.png"),
+	"fb-timeline1.png":    fx.fetch("https://s-static.ak.facebook.com/rsrc.php/v1/yU/r/R8qnmwdGS8-.png"),
+	"fb-timeline2.png":    fx.fetch("https://s-static.ak.facebook.com/rsrc.php/v1/yD/r/N8eF8pLkCCD.png"),
+	"stream-button.png":   fx.load("fb-sprite1.png").darken().crop(0, 454, 64, 480).save().compress(),
+	"timeline-sprite.png": fx.load("fb-timeline1.png").crop(111, 14, 294, 88).colorize({ 'FFFFFF': '353433', 'E8EAF1': '252423', '95A3C2': '8B8685' }).save().compress(),
+	"timeline-bar.png":    fx.load("fb-timeline2.png").darken().save().compress()
 };
 
 function run() {
@@ -233,10 +283,4 @@ function run() {
 }
 
 run();
-
-/*
-var cvs = loadImage(process.argv[2]);
-darken(cvs);
-fs.writeFileSync(process.argv[3], cvs.toBuffer());
-*/
 
