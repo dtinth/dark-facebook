cca41845246b954c0d7b0dad21f9a7acfa7e6d6d
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-11-23 21:06:19 +0700

    add a deployment script

---
+++ scripts/publish-to-userstyle.js
@@ -0,0 +1,42 @@
+
+var casper = require('casper').create()
+var fs = require('fs')
+var system = require('system')
+
+var config = JSON.parse(fs.read(system.env.HOME + '/.dfb2.conf.json'))
+var id = config.userstyles.id
+
+phantom.addCookie({
+  name: 'login',
+  value: config.userstyles.cookie,
+  domain: 'userstyles.org'
+})
+
+casper.start('http://userstyles.org/styles/' + id + '/edit')
+
+casper.then(function() {
+
+  var themeInfo = fs.read('theme-info.txt')
+  var css =
+        '@namespace url(http://www.w3.org/1999/xhtml);\n' +
+        '@-moz-document domain("facebook.com") {\n' +
+        fs.read('dist/dark-facebook.min.css') +
+        '\n}'
+
+  console.log('Filling forms...')
+
+  this.fillSelectors('form[action="/styles/update"]', {
+    '#style_additional_info': themeInfo,
+    '#css':                   css
+  }, false)
+
+  this.click('form[action="/styles/update"] input[type="submit"]')
+
+})
+
+casper.then(function() {
+  console.log(this.getCurrentUrl())
+})
+
+casper.run()
+
