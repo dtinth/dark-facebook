0753be580b3e599b5981d12e215bd8d655965a3d
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-04-08 09:55:49 +0700

    [timeline/page-nav, timeline/unit] Update theme for new "single-column" pages.

--- scripts/generate-description.js
+++ scripts/generate-description.js
@@ -5,6 +5,6 @@ var lift = require('when/node/function').lift
 var exec = lift(require('child_process').exec)
 
 var body = fs.readFileSync('theme-info.txt', 'utf-8')
-exec("git log --format=format:'%ad (%h) %s' --date=short --grep='\\[' -n 20").spread(function(out, err) {
+exec("git log --format=format:'%ad (<code>%h</code>) %s' --date=short --grep='\\[' -n 20").spread(function(out, err) {
   console.log(body.replace('$CHANGELOG$', out))
 })
